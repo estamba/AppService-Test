8beaa4b7bf42b5e4604e3debc37f0a0b998e9adf
estamba <princearbby97@gmail.com>
2020-04-24 16:29:08 +0800

    commit

--- AppService-Test/Controllers/HomeController.cs
+++ AppService-Test/Controllers/HomeController.cs
@@ -6,21 +6,25 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using AppService_Test.Models;
+using Microsoft.Extensions.Configuration;
 
 namespace AppService_Test.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            this.configuration = configuration;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var model = configuration["Greetings"];
+            return View("Index", model);
         }
 
         public IActionResult Privacy()
