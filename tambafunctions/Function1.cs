aa0ae30b083e5a28e31633aca7afa0333a5f8963
estamba <princearbby97@gmail.com>
2020-04-28 17:20:34 +0800

    commit

--- tambafunctions/Function1.cs
+++ tambafunctions/Function1.cs
@@ -26,6 +26,7 @@ namespace tambafunctions
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
 
+
             return name != null
                 ? (ActionResult)new OkObjectResult($"Hello, {name}")
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
